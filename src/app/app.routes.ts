0443292598b3a25ab9b3aed46a31a52cf87881b9
yovanilopez <89365247+yovanilopez@users.noreply.github.com>
2024-08-23 21:04:46 -0600

    primaria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import path from 'node:path';
 import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
 import { PeritocompuComponent } from './components/peritocompu/peritocompu.component';
 import { BchilleratocompuComponent } from './components/bchilleratocompu/bchilleratocompu.component';
+import { PrimariaComponent } from './components/primaria/primaria.component';
 
 export const routes: Routes = [
     {path:"", redirectTo:"bienvenida" ,pathMatch:"full"} ,
     {path:"bienvenida", component:BienvenidaComponent } ,
     {path:"4toperitocompu", component:PeritocompuComponent } ,
     {path:"4tobachillcompu", component:BchilleratocompuComponent } ,
+    {path:"primaria", component:PrimariaComponent } ,
 
 ];
