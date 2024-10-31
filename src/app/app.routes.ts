8107ca16041f97e69e6a944f7f9336648de314c5
yovanilopez <89365247+yovanilopez@users.noreply.github.com>
2024-10-31 14:47:37 -0600

    inicio cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { BienvenidaComponent } from './components/bienvenida/bienvenida.componen
 import { PeritocompuComponent } from './components/peritocompu/peritocompu.component';
 import { BchilleratocompuComponent } from './components/bchilleratocompu/bchilleratocompu.component';
 import { PrimariaComponent } from './components/primaria/primaria.component';
+import { FinsemanaComponent } from './components/finsemana/finsemana.component';
 
 export const routes: Routes = [
     {path:"", redirectTo:"bienvenida" ,pathMatch:"full"} ,
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path:"4toperitocompu", component:PeritocompuComponent } ,
     {path:"4tobachillcompu", component:BchilleratocompuComponent } ,
     {path:"primaria", component:PrimariaComponent } ,
+    {path:"finsemana", component:FinsemanaComponent } ,
 
 ];
